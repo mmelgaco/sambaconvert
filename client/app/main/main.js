0d36e3026cff74b910b1892f67c7868047b2a74d
mmelgaco <marcelo@rapidoware.com.br>
2015-12-02 10:15:04 -0200

    initial project basis

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('sambaconvertApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
