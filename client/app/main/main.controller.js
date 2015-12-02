551409586b17df5c6c8340eba7d4bcf9550c1f8b
mmelgaco <marcelo@rapidoware.com.br>
2015-12-02 11:22:18 -0200

    S3 Upload working

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,21 +2,47 @@
 
 angular.module('sambaconvertApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+        $scope.creds = {
+            bucket: 'sambaconvert',
+            access_key: 'AKIAJ2TWN4ETEQMASDZA',
+            secret_key: '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'
+        };
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
+        $scope.upload = function() {
+            // Configure The S3 Object
+            AWS.config.update({ accessKeyId: $scope.creds.access_key, secretAccessKey: $scope.creds.secret_key });
+            //AWS.config.region = 'us-east-1';
+            var bucket = new AWS.S3({ params: { Bucket: $scope.creds.bucket } });
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+            if($scope.file) {
+                var params = { ACL: 'public-read', Key: $scope.file.name, ContentType: $scope.file.type, Body: $scope.file, ServerSideEncryption: 'AES256' };
+
+                bucket.putObject(params, function(err, data) {
+                    if(err) {
+                        // There Was An Error With Your S3 Config
+                        alert(err.message);
+                        return false;
+                    }else {
+                        // Success!
+                        alert('Upload Done');
+
+                        var url = 'https://s3.amazonaws.com/' + $scope.creds.bucket + '/' + $scope.file.name;
+                        console.log(url);
+
+                        $scope.fileUrl = url;
+                    }
+                }).on('httpUploadProgress',function(progress) {
+                    // Log Progress Information
+                    console.log(Math.round(progress.loaded / progress.total * 100) + '% done');
+
+                    $scope.uploadProgress = Math.round(progress.loaded / progress.total * 100);
+                    $scope.$digest();
+                });
+            }else {
+                // No File Selected
+                alert('No File Selected');
+            }
+        }
+
   });
